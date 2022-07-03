37dee78170d71655d553091c6e7ec06ac7ee2151
Godfrey Jonas <41231839+jonasgodfrey@users.noreply.github.com>
2022-07-03 15:09:48 +0100

    Latest edits

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -12,7 +12,7 @@
                     <div class="page-title-box page-title-box-alt">
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">MyTenants MIS</a></li>
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Sporty Bang</a></li>
                                 <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
                             </ol>
                         </div>
@@ -24,7 +24,7 @@
 
             <div class="row">
 
-                <div class="col-xl-3 col-md-6">
+                <div class="col-xl-4 col-md-6">
                     <div class="card">
                         <div class="card-body">
                             <div class="dropdown float-end">
@@ -35,7 +35,7 @@
 
                             </div>
 
-                            <h4 class="header-title mt-0 mb-4">Total Properties</h4>
+                            <h4 class="header-title mt-0 mb-4">Total Investments</h4>
 
                             <div class="widget-chart-1">
                                 <div class="widget-chart-box-1 float-start" dir="ltr">
@@ -46,14 +46,14 @@
 
 
                                 <div class="widget-detail-1 text-end">
-                                    <h2 class="fw-normal pt-2 mb-1"> {{ $properties }} </h2>
+                                    <h2 class="fw-normal pt-2 mb-1"> ₦{{ $properties }} </h2>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div><!-- end col -->
 
-                <div class="col-xl-3 col-md-6">
+                <div class="col-xl-4 col-md-6">
                     <div class="card">
                         <div class="card-body">
                             <div class="dropdown float-end">
@@ -64,7 +64,7 @@
 
                             </div>
 
-                            <h4 class="header-title mt-0 mb-4">Total Units</h4>
+                            <h4 class="header-title mt-0 mb-4">Total Earnings</h4>
 
                             <div class="widget-chart-1">
                                 <div class="widget-chart-box-1 float-start" dir="ltr">
@@ -75,13 +75,13 @@
 
 
                                 <div class="widget-detail-1 text-end">
-                                    <h2 class="fw-normal pt-2 mb-1"> {{ $units }} </h2>
+                                    <h2 class="fw-normal pt-2 mb-1"> ₦{{ $units }} </h2>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div><!-- end col -->
-                <div class="col-xl-3 col-md-6">
+                <div class="col-xl-4 col-md-6">
                     <div class="card">
                         <div class="card-body">
                             <div class="dropdown float-end">
@@ -92,7 +92,7 @@
 
                             </div>
 
-                            <h4 class="header-title mt-0 mb-4">Total Tenants</h4>
+                            <h4 class="header-title mt-0 mb-4">Total Referrals</h4>
 
                             <div class="widget-chart-1">
                                 <div class="widget-chart-box-1 float-start" dir="ltr">
@@ -109,175 +109,13 @@
                         </div>
                     </div>
                 </div><!-- end col -->
-                <div class="col-xl-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="dropdown float-end">
-                                <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical"></i>
-                                </a>
-
-                            </div>
-
-                            <h4 class="header-title mt-0 mb-4">Locations</h4>
-
-                            <div class="widget-chart-1">
-                                <div class="widget-chart-box-1 float-start" dir="ltr">
-                                    <input data-plugin="knob" data-width="70" data-height="70" data-fgColor="#f05050 "
-                                        data-bgColor="#00FF12" value="{{ $properties }}" data-skin="tron"
-                                        data-angleOffset="180" data-readOnly=true data-thickness=".15" />
-                                </div>
-
-
-                                <div class="widget-detail-1 text-end">
-                                    <h2 class="fw-normal pt-2 mb-1"> {{ $properties }} </h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- end col -->
+                
 
             </div>
-            <!-- end row -->
+            <!-- end row -->            
 
 
-            <div class="row">
-                <div class="col-xl-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body widget-user">
-                            <div class="text-center">
-                                <h2 class="fw-normal text-primary" data-plugin="counterup">N0</h2>
-                                <h5>Total Rents</h5>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="col-xl-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body widget-user">
-                            <div class="text-center">
-                                <h2 class="fw-normal text-pink" data-plugin="counterup">N0</h2>
-                                <h5>Total Fees</h5>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="col-xl-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body widget-user">
-                            <div class="text-center">
-                                <h2 class="fw-normal text-warning" data-plugin="counterup">N0</h2>
-                                <h5>Total Taxes</h5>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="col-xl-3 col-md-6">
-                    <div class="card">
-                        <div class="card-body widget-user">
-                            <div class="text-center">
-                                <h2 class="fw-normal text-info" data-plugin="counterup">N0</h2>
-                                <h5>Total</h5>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <!-- end row -->
-
-
-            <div class="row">
-                <div class="col-xl-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="dropdown float-end">
-                                <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical"></i>
-                                </a>
-
-                            </div>
-
-                            <h4 class="header-title mt-0">Tenants Rents</h4>
-
-                            <div class="widget-chart text-center">
-                                <div id="morris-donut-example" dir="ltr" style="height: 245px;" class="morris-chart">
-                                </div>
-                                <ul class="list-inline chart-detail-list mb-0">
-                                    <li class="list-inline-item">
-                                        <h5 style="color: #ff8acc;"><i class="fa fa-circle me-1"></i>Paid</h5>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <h5 style="color: #5b69bc;"><i class="fa fa-circle me-1"></i>Unpaid</h5>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <h5 style="color: #35B8E0;"><i class="fa fa-circle me-1"></i>Overdue</h5>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- end col -->
-
-                <div class="col-xl-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="dropdown float-end">
-                                <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical"></i>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item">Action</a>
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item">Another action</a>
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item">Something else</a>
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item">Separated link</a>
-                                </div>
-                            </div>
-                            <h4 class="header-title mt-0">Statistics</h4>
-                            <div id="morris-bar-example" dir="ltr" style="height: 280px;" class="morris-chart"></div>
-                        </div>
-                    </div>
-                </div><!-- end col -->
-
-                <div class="col-xl-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="dropdown float-end">
-                                <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    <i class="mdi mdi-dots-vertical"></i>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item">Action</a>
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item">Another action</a>
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item">Something else</a>
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item">Separated link</a>
-                                </div>
-                            </div>
-                            <h4 class="header-title mt-0">Total Revenue</h4>
-                            <div id="morris-line-example" dir="ltr" style="height: 280px;" class="morris-chart"></div>
-                        </div>
-                    </div>
-                </div><!-- end col -->
-            </div>
-            <!-- end row -->
+            
 
 
             <div class="row">
@@ -302,14 +140,14 @@
                                 </div>
                             </div>
 
-                            <h4 class="header-title mt-0 mb-3">Tenants</h4>
+                            <h4 class="header-title mt-0 mb-3">Payment History</h4>
 
                             <div class="table-responsive">
                                 <table id="datatable-buttons" class="table table-hover mb-0 dt-responsive nowrap">
                                     <thead>
                                         <tr>
                                             <th>#</th>
-                                            <th>Property</th>
+                                            <th>Investor</th>
                                             <th>Unit</th>
                                             <th>Tenant</th>
                                             <th>Tenant Tel</th>
