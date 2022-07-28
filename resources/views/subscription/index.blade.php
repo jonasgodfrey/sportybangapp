372e68807ab8515500600733175ed2805817c9cb
Godfrey Jonas <41231839+jonasgodfrey@users.noreply.github.com>
2022-07-28 16:31:36 +0100

    worked on subscription page

--- resources/views/subscription/index.blade.php
+++ resources/views/subscription/index.blade.php
@@ -41,7 +41,7 @@
                                     <div class="card">
                                         <div class="inner-box card-body">
                                             <div class="plan-header p-3 text-center">
-                                                <h3 class="plan-title">Step-up stage</h3>
+                                                <h3 class="plan-title">Basic</h3>
                                                 <h2 class="plan-title">₦5,000.00</h2>
                                                 <div class="plan-duration">Per Week</div>
                                             </div>
@@ -87,7 +87,7 @@
                                     <div class="card">
                                         <div class="inner-box card-body">
                                             <div class="plan-header p-3 text-center">
-                                                <h3 class="plan-title">Up-Grade Stage</h3>
+                                                <h3 class="plan-title">Standard</h3>
                                                 <h2 class="plan-title">₦10,000.00</h2>
                                                 <div class="plan-duration">Per Week</div>
                                             </div>
@@ -132,7 +132,7 @@
                                     <div class="card">
                                         <div class="inner-box card-body">
                                             <div class="plan-header p-3 text-center">
-                                                <h3 class="plan-title">Income-Stage</h3>
+                                                <h3 class="plan-title">Classic</h3>
                                                 <h2 class="plan-title">₦20,000.00</h2>
                                                 <div class="plan-duration">Per Week</div>
                                             </div>
@@ -173,11 +173,11 @@
 
                                 <!--Pricing Column-->
                                 <article class="pricing-column col-xl-3 col-md-6">
-                                    <div class="ribbon"><span>EXECUTIVE</span></div>
+                                    <div class="ribbon"><span>Premium</span></div>
                                     <div class="card">
                                         <div class="inner-box card-body">
                                             <div class="plan-header p-3 text-center">
-                                                <h3 class="plan-title">Capital-Stage</h3>
+                                                <h3 class="plan-title">PREMIUM</h3>
                                                 <h2 class="plan-title">₦50,000.00</h2>
                                                 <div class="plan-duration">Per Week</div>
                                             </div>
@@ -215,6 +215,140 @@
                                         </div>
                                     </div>
                                 </article>
+
+                                <article class="pricing-column col-xl-4 col-md-6">
+                                    <div class="ribbon"><span>Premium</span></div>
+                                    <div class="card">
+                                        <div class="inner-box card-body">
+                                            <div class="plan-header p-3 text-center">
+                                                <h3 class="plan-title">SILVER</h3>
+                                                <h2 class="plan-title">₦70,000.00</h2>
+                                                <div class="plan-duration">Per Week</div>
+                                            </div>
+                                            <ul class="plan-stats list-unstyled text-center p-3 mb-0">
+                                                <li>30% Returns</li>
+                                                <li>Weekly Basis</li>
+                                            </ul>
+                                            <form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal"
+                                                  role="form">
+
+                                                <div class="col-md-8 col-md-offset-2">
+                                                    <input type="hidden" name="email"
+                                                           value="{{Auth::user()->email}}"> {{-- required --}}
+                                                    <input type="hidden" name="amount" class="form-control" id="amount" value="5000000">
+                                                    <input type="hidden" name="orderID" value="345">
+                                                    <input type="hidden" name="currency" value="NGN">
+                                                    <input type="hidden" name="metadata"
+                                                           value="{{ json_encode($array = ['plan_type' => '4'], JSON_THROW_ON_ERROR) }}">{{-- For other necessary things you want to add to your payload. it is optional though --}}
+                                                    <input type="hidden" name="reference"
+                                                           value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
+                                                    <input type="hidden" name="callback_url" class="form-control"
+                                                           value="{{route('handleGatewayCallback')}}">
+                                                    <input type="hidden" name="_token"
+                                                           value="{{ csrf_token() }}"> {{-- employ this in place of csrf_field only in laravel 5.0 --}}
+
+                                                </div>
+                                                <div class="text-center">
+                                                    <button class="btn  btn-bordered-success btn-success rounded-pill waves-effect waves-light" type="submit" value="Pay Now!">
+                                                        <i class="fa fa-plus-circle fa-lg"></i> Pay Now!
+                                                    </button>
+                                                </div>
+                                            </form>
+
+
+                                        </div>
+                                    </div>
+                                </article>
+
+                                <article class="pricing-column col-xl-4 col-md-6">
+                                    <div class="ribbon"><span>Diamond</span></div>
+                                    <div class="card">
+                                        <div class="inner-box card-body">
+                                            <div class="plan-header p-3 text-center">
+                                                <h3 class="plan-title">DIAMOND</h3>
+                                                <h2 class="plan-title">₦100,000.00</h2>
+                                                <div class="plan-duration">Per Week</div>
+                                            </div>
+                                            <ul class="plan-stats list-unstyled text-center p-3 mb-0">
+                                                <li>30% Returns</li>
+                                                <li>Weekly Basis</li>
+                                            </ul>
+                                            <form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal"
+                                                  role="form">
+
+                                                <div class="col-md-8 col-md-offset-2">
+                                                    <input type="hidden" name="email"
+                                                           value="{{Auth::user()->email}}"> {{-- required --}}
+                                                    <input type="hidden" name="amount" class="form-control" id="amount" value="5000000">
+                                                    <input type="hidden" name="orderID" value="345">
+                                                    <input type="hidden" name="currency" value="NGN">
+                                                    <input type="hidden" name="metadata"
+                                                           value="{{ json_encode($array = ['plan_type' => '4'], JSON_THROW_ON_ERROR) }}">{{-- For other necessary things you want to add to your payload. it is optional though --}}
+                                                    <input type="hidden" name="reference"
+                                                           value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
+                                                    <input type="hidden" name="callback_url" class="form-control"
+                                                           value="{{route('handleGatewayCallback')}}">
+                                                    <input type="hidden" name="_token"
+                                                           value="{{ csrf_token() }}"> {{-- employ this in place of csrf_field only in laravel 5.0 --}}
+
+                                                </div>
+                                                <div class="text-center">
+                                                    <button class="btn  btn-bordered-success btn-success rounded-pill waves-effect waves-light" type="submit" value="Pay Now!">
+                                                        <i class="fa fa-plus-circle fa-lg"></i> Pay Now!
+                                                    </button>
+                                                </div>
+                                            </form>
+
+
+                                        </div>
+                                    </div>
+                                </article>
+
+                                <article class="pricing-column col-xl-4 col-md-6">
+                                    <div class="ribbon"><span>Gold</span></div>
+                                    <div class="card">
+                                        <div class="inner-box card-body">
+                                            <div class="plan-header p-3 text-center">
+                                                <h3 class="plan-title">GOLD</h3>
+                                                <h2 class="plan-title">₦200,000.00</h2>
+                                                <div class="plan-duration">Per Week</div>
+                                            </div>
+                                            <ul class="plan-stats list-unstyled text-center p-3 mb-0">
+                                                <li>30% Returns</li>
+                                                <li>Weekly Basis</li>
+                                            </ul>
+                                            <form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal"
+                                                  role="form">
+
+                                                <div class="col-md-8 col-md-offset-2">
+                                                    <input type="hidden" name="email"
+                                                           value="{{Auth::user()->email}}"> {{-- required --}}
+                                                    <input type="hidden" name="amount" class="form-control" id="amount" value="5000000">
+                                                    <input type="hidden" name="orderID" value="345">
+                                                    <input type="hidden" name="currency" value="NGN">
+                                                    <input type="hidden" name="metadata"
+                                                           value="{{ json_encode($array = ['plan_type' => '4'], JSON_THROW_ON_ERROR) }}">{{-- For other necessary things you want to add to your payload. it is optional though --}}
+                                                    <input type="hidden" name="reference"
+                                                           value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
+                                                    <input type="hidden" name="callback_url" class="form-control"
+                                                           value="{{route('handleGatewayCallback')}}">
+                                                    <input type="hidden" name="_token"
+                                                           value="{{ csrf_token() }}"> {{-- employ this in place of csrf_field only in laravel 5.0 --}}
+
+                                                </div>
+                                                <div class="text-center">
+                                                    <button class="btn  btn-bordered-success btn-success rounded-pill waves-effect waves-light" type="submit" value="Pay Now!">
+                                                        <i class="fa fa-plus-circle fa-lg"></i> Pay Now!
+                                                    </button>
+                                                </div>
+                                            </form>
+
+
+                                        </div>
+                                    </div>
+                                </article>
+
+                           
 
 
                             </div><!-- end row -->
